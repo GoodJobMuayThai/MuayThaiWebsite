bbaeb8d57b3b3201af68518c2382dafd4b03ff2b
kevvie <k.drago57@gmail.com>
2025-07-19 16:00:28 +1000

    fix build by removing experimental options

--- next.config.mjs
+++ next.config.mjs
@@ -15,9 +15,6 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   generateEtags: false,
-  experimental: {
-    optimizeCss: true,
-  },
   async headers() {
     return [
       {
